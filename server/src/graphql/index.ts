91860a7fbe1270a837ad0696e36672ed89023e59
IuriiSer <yusergeevi4@gmail.com>
2022-10-15 14:51:34 +0300

    all schemas and resolvers builder

---
+++ server/src/graphql/index.ts
@@ -0,0 +1,8 @@
+import { DocumentNode } from 'graphql';
+import { IExecutableSchemaDefinition } from '@graphql-tools/schema';
+
+import CharacterSchema from './character/CharacterSchema';
+import CharacterResolvers  from './character/CharacterResolver'
+
+export const Schemas: DocumentNode[] = [CharacterSchema];
+export const Resolvers: IExecutableSchemaDefinition['resolvers'] = [CharacterResolvers];
